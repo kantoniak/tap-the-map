e7a0073c42d42319721925b87efedec664ab83d7
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-04-19 21:34:05 +0200

    Add SplitTestActivity

---
+++ app/src/main/java/com/kantoniak/discrete_fox/SplitTestActivity.java
@@ -0,0 +1,21 @@
+package com.kantoniak.discrete_fox;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplitTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_test);
+    }
+}
